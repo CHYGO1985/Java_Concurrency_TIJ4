3e8c91c7418911b5d498e5b7dded0cf94602401f
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-07-14 17:10:48 +0100

    /**  *  * Exercise 2. TIJ4 Concurrency.  *  * @author jingjiejiang  * @history  * 1. Jul 14, 2017  */

---
+++ TIJ4Exercise2/src/TIJ4Exercise2.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ * Exercise 2. TIJ4 Concurrency.
+ * 
+ * @author jingjiejiang
+ * @history
+ * 1. Jul 14, 2017
+ */
+public class TIJ4Exercise2 {
+	
+	public static class FibonacciTask implements Runnable {
+
+		private int count;
+		
+		public FibonacciTask() {
+			count = 0;
+		}
+		
+		public FibonacciTask(int count) {
+			this.count = count;
+		}
+		
+		@Override
+		public void run() {
+			Fibonacci num = new Fibonacci();
+			for (int i = 0; i < count; i ++) {
+				System.out.print(num.fib(i) + "\t");
+				if (i > 0  && i % 4 == 0) System.out.println();
+			}
+			// System.out.println();
+		}
+		
+		public class Fibonacci {
+			private int fib(int n) {
+				if(n < 2) return 1;
+				return fib(n-2) + fib(n-1);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 8; i ++) {
+			Thread t = new Thread(new FibonacciTask(i));
+			t.start();
+			//System.out.println(t + "finished. **********");
+		}
+	}
+}
